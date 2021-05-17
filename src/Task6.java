39f4c95859fd181aaf61b31dcd0d729fe99e5cba
EskimosEretik <messalina1960@gmail.com>
2021-05-17 20:53:03 +0300

    Task6

---
+++ src/Task6.java
@@ -0,0 +1,10 @@
+public class Task6 {
+    public static void main(String[] args) {
+        final int a = 10;
+        final int b = 15;
+        double p = 2*(a+b);
+        double s = a*b;
+        System.out.println(p);
+        System.out.println(s);
+    }
+}
