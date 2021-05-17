c4c397b80f2838e17942b77d8e14088fc77ac6fd
EskimosEretik <messalina1960@gmail.com>
2021-05-17 20:47:52 +0300

    Task5

---
+++ src/Task5.java
@@ -0,0 +1,9 @@
+public class Task5 {
+    public static void main(String[] args) {
+       final double r = 8;
+       double l = 2 * Math.PI * r;
+       double s = Math.PI * r * r;
+        System.out.println(l);
+        System.out.println(s);
+    }
+}
