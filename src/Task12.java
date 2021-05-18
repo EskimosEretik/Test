9879660999e804a2f563bf916c7ea30a6e16b54e
EskimosEretik <messalina1960@gmail.com>
2021-05-18 14:39:40 +0300

    Task12

--- src/Task12.java
+++ src/Task12.java
@@ -5,22 +5,21 @@ public class Task12 {
         Scanner in = new Scanner(System.in);
         int count = in.nextInt();
         int pie = 0;
-        for (int i=0; i<count; i++)
-        {
+        for (int i = 0; i < count; i++) {
             pie++;
         }
         int pack = 0;
 
         System.out.println("если 100% учеников весят меньше 30кг: ");
-        pack = (int)Math.ceil((pie*0.2)/0.9);
-        System.out.println(pack+ " " + pie * 2);
+        pack = (int) Math.ceil((pie * 0.2) / 0.9);
+        System.out.println(pack + " " + pie * 2);
 
         System.out.println("если 60% учеников весят меньше 30кг: ");
-        pack = (int)Math.ceil((pie*0.6*0.2)/0.9);
-        System.out.println(pack+ " " + (pie + (int)Math.ceil(pie*0.6)));
+        pack = (int) Math.ceil((pie * 0.6 * 0.2) / 0.9);
+        System.out.println(pack + " " + (pie + (int) Math.ceil(pie * 0.6)));
 
         System.out.println("если 1% учеников весят меньше 30кг: ");
-        pack = (int)Math.ceil((pie*0.01*0.2)/0.9);
-        System.out.println(pack+ " " + (pie + (int)Math.ceil(pie*0.01)));
+        pack = (int) Math.ceil((pie * 0.01 * 0.2) / 0.9);
+        System.out.println(pack + " " + (pie + (int) Math.ceil(pie * 0.01)));
     }
 }
